55e2a0c350033c4ea589f5fd786ae2d433af6c5b
Juan Paulo Gutierrez <juanpaulo.gutierrez@gmail.com>
2016-04-20 15:58:10 +0900

    Lint cleanup.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,5 @@
-'use strict';
+// 'use strict';
 
-// import {should} from 'chai';
 const should = require('chai').should();
 
 const Hapi = require('hapi');
@@ -8,7 +7,7 @@ const MongoPlug = require('../lib');
 
 describe('MongoPlug', () => {
 
-  describe('#register', (done) => {
+  describe('Connecting', (done) => {
 
     let server;
 
@@ -17,6 +16,12 @@ describe('MongoPlug', () => {
       done();
     });
 
+    // afterEach((done) => {
+    //   server.db.close();
+    //   done();
+    // });
+
+    // MongoDB connection
     it('should connect to mongodb://localhost:27017/ by default', (done) => {
       server.register({
         register: MongoPlug
@@ -34,7 +39,7 @@ describe('MongoPlug', () => {
         }
       }, (err) => {
         should.exist(err);
-        err.should.have.property('name').equal('MongoError')
+        err.should.have.property('name').equal('MongoError');
         done();
       });
     });
@@ -49,11 +54,86 @@ describe('MongoPlug', () => {
       });
     });
 
-    // it('should do in an exposed db object', (done) => {
-    // });
+  });
 
-    describe('#attributes', () => {
-      // TODO
+  describe('CRUD Operations', (done) => {
+
+    let server;
+
+    beforeEach((done) => {
+      server = new Hapi.Server();
+      server.register({
+        register: MongoPlug
+      }, (err) => {
+        done();
+      });
     });
+
+    // Inserting Documents
+    it('should be able to create a single document', (done) => {
+      server.db.collection('test').insertOne({foo: 'bar'}, (err, doc) => {
+        should.exist(doc);
+        doc.should.have.deep.property('result.ok').equal(1);
+        doc.should.have.deep.property('result.n').equal(1);
+        // {foo: 'bar'}
+        done();
+      });
+    });
+
+    it('should be able to create multiple documents', (done) => {
+      server.db.collection('test').insertMany([{foo: 'bar'}, {foo: 'baz'}], (err, doc) => {
+        doc.should.have.deep.property('result.ok').equal(1);
+        doc.should.have.deep.property('result.n').equal(2);
+        // {foo: 'bar'}
+        // {foo: 'bar'}
+        // {foo: 'baz'}
+        done();
+      });
+    });
+
+    // Updating Documents
+    it('should be able to update a single document', (done) => {
+      server.db.collection('test').updateOne({foo: 'bar'}, {$set: {foo: 'baz'}}, (err, doc) => {
+        doc.should.have.deep.property('result.ok').equal(1);
+        doc.should.have.deep.property('result.nModified').equal(1);
+        done();
+        // {foo: 'bar'}
+        // {foo: 'baz'}
+        // {foo: 'baz'}
+      });
+    });
+
+    it('should be able to update multiple documents', (done) => {
+      server.db.collection('test').updateMany({foo: 'baz'}, {$set: {foo: 'bar'}}, (err, doc) => {
+        should.exist(doc);
+        doc.should.have.deep.property('result.ok').equal(1);
+        doc.should.have.deep.property('result.nModified').equal(2);
+        done();
+        // {foo: 'bar'}
+        // {foo: 'bar'}
+        // {foo: 'bar'}
+      });
+    });
+
+    it('should be able to remove a single document', (done) => {
+      server.db.collection('test').deleteOne({foo: 'bar'}, (err, doc) => {
+        should.exist(doc);
+        doc.should.have.deep.property('result.ok').equal(1);
+        doc.should.have.deep.property('result.n').equal(1);
+        done();
+        // {foo: 'bar'}
+        // {foo: 'bar'}
+      });
+    });
+
+    it('should be able to remove multiple documents', (done) => {
+      server.db.collection('test').deleteMany({foo: 'bar'}, (err, doc) => {
+        should.exist(doc);
+        doc.should.have.deep.property('result.ok').equal(1);
+        doc.should.have.deep.property('result.n').equal(2);
+        done();
+      });
+    });
+
   });
 });
